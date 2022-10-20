83da8983b1c88f4e467931c11ebb2dfb05bf26e3
yakoob <yakoobusef@gmail.com>
2022-10-20 09:38:44 +0300

    create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Home Care services store</a>
+      </header>
+      <main>list services </main>
+    </div>
+  );
+}
+
+export default App;
