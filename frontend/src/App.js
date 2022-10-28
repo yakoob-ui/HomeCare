091b820ac65235c3d66ee4b5104e028fc6c06c54
yakoob <yakoobusef@gmail.com>
2022-10-28 16:17:29 +0300

    commit 10

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import HomeScreen from './screens/HomeScren';
