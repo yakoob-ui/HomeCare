6f0800ff753d131089d03dbf9849979180c085a6
yakoob <yakoobusef@gmail.com>
2022-10-29 00:13:33 +0300

    commit 13

--- backend/routes/serviceRoutes.js
+++ backend/routes/serviceRoutes.js
@@ -9,6 +9,82 @@ serviceRouter.get('/', async (req, res) => {
   res.send(services);
 });
 
+const PAGE_SIZE = 3;
+serviceRouter.get(
+  '/search',
+  expressAsyncHandler(async (req, res) => {
+    const { query } = req;
+    const pageSize = query.pageSize || PAGE_SIZE;
+    const page = query.page || 1;
+    const category = query.category || '';
+    const price = query.price || '';
+    const rating = query.rating || '';
+    const order = query.order || '';
+    const searchQuery = query.query || '';
+
+    const queryFilter =
+      searchQuery && searchQuery !== 'all'
+        ? {
+            name: {
+              $regex: searchQuery,
+              $options: 'i',
+            },
+          }
+        : {};
+    const categoryFilter = category && category !== 'all' ? { category } : {};
+    const ratingFilter =
+      rating && rating !== 'all'
+        ? {
+            rating: {
+              $gte: Number(rating),
+            },
+          }
+        : {};
+    const priceFilter =
+      price && price !== 'all'
+        ? {
+            // 1-50
+            price: {
+              $gte: Number(price.split('-')[0]),
+              $lte: Number(price.split('-')[1]),
+            },
+          }
+        : {};
+    const sortOrder =
+      order === 'featured'
+        ? { featured: -1 }
+        : order === 'lowest'
+        ? { price: 1 }
+        : order === 'highest'
+        ? { price: -1 }
+        : order === 'toprated'
+        ? { rating: -1 }
+        : order === 'newest'
+        ? { createdAt: -1 }
+        : { _id: -1 };
+    const dbQuery = {
+      ...queryFilter,
+      ...categoryFilter,
+      ...priceFilter,
+      ...ratingFilter,
+    };
+    console.log('dbQuery', dbQuery);
+    const services = await Service.find(dbQuery)
+      .sort(sortOrder)
+      .skip(pageSize * (page - 1))
+      .limit(pageSize);
+    console.log('services', services);
+
+    const countServices = await Service.countDocuments(dbQuery);
+    res.send({
+      services,
+      countServices,
+      page,
+      pages: Math.ceil(countServices / pageSize),
+    });
+  })
+);
+
 serviceRouter.get(
   '/categories',
   expressAsyncHandler(async (req, res) => {
